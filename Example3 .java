1f01d1b3867808bb12803c936570ddc93e1a7f97
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 14:42:22 +0530

    Create Example3 .java
    incremented and decremented oparator example

---
+++ Example3 .java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Example3 {
+
+	public static void main(String[] args) {
+      //++ , -- Post/Pre
+		int i=5,j=0;
+	//	i++;//i=i+1 Post increment
+	//	++i;//i=i+1 Pre increment
+	//	i--;//i=i-1 Post decrement
+	//	--i;//i=i-1 Pre decrement
+	//	j=i++;//step 1:j=i step 2:i=i+1
+	//	j=++i;//step 1:i=i+1 step 2:j=i
+	//	j=i--;//step 1:j=i step 2:i=i-1
+	//  j=--i;//step 1:i=i-1 step 2:j=i
+		System.out.println("i:"+i+" j:"+j);
+	}
+}
