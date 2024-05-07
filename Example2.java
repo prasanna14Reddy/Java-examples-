e9857ffbe84a14ad0bd95ffe7c47087a38d3e5fd
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 14:35:17 +0530

    Create Example2.java

---
+++ Example2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Example2 {
+
+	public static void main(String[] args) {
+      int a=5;//assignment
+      a+=5;//a=a+5
+      System.out.println(a);//a=10
+      a-=5;//a=a-5
+      System.out.println(a);//a=5
+      a*=5;//a=a*5
+      System.out.println(a);//a=25
+      a/=5;//a=a/5
+      System.out.println(a);//a=5
+      a%=5;//a=a%5
+      System.out.println(a);//a=0
+	}
+
+}
