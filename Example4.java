102c74c91d05ba049ac9924073bfdaf3c3298ee5
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 14:48:49 +0530

    Create Example4.java
    Comparison operators example program

---
+++ Example4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Example4 {
+
+	public static void main(String[] args) {
+       int age=20;
+       String st="yes",st1="no";
+      String res= (age>18)?st:st1;
+      System.out.println(res);
+	}
+
+}
