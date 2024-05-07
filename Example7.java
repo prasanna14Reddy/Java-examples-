b3c782d2e50df9a160255f53539ab7b3eed84528
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 15:02:18 +0530

    Create Example7.java
    Shift oparator example program

---
+++ Example7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Example7
+{
+
+	public static void main(String[] args) {
+       
+		byte a=5;
+		int x,y;
+		x=a<<2;
+		y=a>>2;
+		System.out.println("x:"+x+" y:"+y);
+    	   
+	}
+}
