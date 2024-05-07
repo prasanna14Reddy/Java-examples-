2808c2da02e0924b4939b0b4294e04a6bd240e15
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 14:51:54 +0530

    Create  Example5 .java
    logical comparison operator example program

---
+++ Example5 .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Example5 {
+
+	public static void main(String[] args) {
+       int a=10,b=5;
+       //<,>,<=,>=,==,!=
+       System.out.println(a<b);
+       System.out.println(a>b);
+       System.out.println(a<=b);
+       System.out.println(a>=b);
+       System.out.println(a==b);
+       System.out.println(a!=b);
+	}
+
+}
