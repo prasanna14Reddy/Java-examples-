56e1c776d3d88490451d523dc54d7f6014d5d175
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 15:38:15 +0530

    Create Whileexample.java
    while control statement example program

---
+++ Whileexample.java
@@ -0,0 +1,17 @@
+public class Whileexample {
+
+	public static void main(String[] args) {
+		int a=15,b=15;
+		while(a<10)
+		{
+			a++;
+			System.out.print(a+"  ");
+		}
+		System.out.println("\n---------------");
+		do 
+		{
+			b++;
+			System.out.print(b+"  ");
+		}while(b<10);
+	}
+}
