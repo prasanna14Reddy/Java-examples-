913da3ca7aad38ff280cee39e8d33b31270b3e1c
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-07 16:29:45 +0530

    Create BookMain.java
    the default static method example program

---
+++ BookMain.java
@@ -0,0 +1,11 @@
+public class BookMain 
+{
+
+	public static void main(String[] args) {
+		Book bm1 = new Book(101,"aaa");
+		Book bm2 = new Book(102,"bbb");
+		System.out.println(bm1);//bm1.toString()
+		System.out.println(bm2);//bm2.toString()
+	}
+
+}
