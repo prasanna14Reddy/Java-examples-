0d07228247df5b17da1dee4ff2a66db4c4402143
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-10 07:22:23 +0530

    Create Asciivalues.java
    Finding ASCII value of a character

---
+++ Asciivalues.java
@@ -0,0 +1,33 @@
+class Asciivalues
+
+{
+
+public static void main(String args[])
+
+}-
+
+//assigning a character constant to an integer
+
+int a=^ prime R' ;
+
+int b=^ prime p' ;
+
+System.out.println(a);
+
+System.out.println(b);
+
+char ch;
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter character:");
+
+ch=sc.next().charAt(0);
+
+int a=(int)ch;
+
+System.out.println("ASCII of " ch + is "+a);
+
+}
+
+}
