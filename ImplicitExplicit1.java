8c557b9509b83fdc84f4a8bde0127bdb17c32573
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-10 07:21:01 +0530

    Create ImplicitExplicit1.java
    Implicit and Explicit typecasting example program

---
+++ ImplicitExplicit1.java
@@ -0,0 +1,27 @@
+class ImplicitExplicit1
+
+{
+
+public static void main(String args[])
+
+{
+
+//implicit
+
+int a = 11
+
+double b = a
+
+System.out.println(a);
+
+System.out.println(b);
+
+//explicit
+
+float d = 15.77f
+
+double f = d
+
+}
+
+}
