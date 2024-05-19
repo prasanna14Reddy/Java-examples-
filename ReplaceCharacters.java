0824a02cacdca4065323f008b1fc58a2d936b951
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-19 17:34:25 +0530

    Create ReplaceCharacters.java
    Replacing a string

---
+++ ReplaceCharacters.java
@@ -0,0 +1,15 @@
+public class ReplaceCharacters 
+{
+
+public static void main(String[] args) {
+
+String originalString = "Hello Everyone" ;
+
+String replacedString originalString .replace('e', 'a');
+
+System.out.println("Original String: " + originalString);
+
+System.out.println("Replaced String: replacedString);
+
+}
+}
