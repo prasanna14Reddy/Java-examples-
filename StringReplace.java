d4ae7bb9102e86090fee1926c2d0e321e2ebfaa0
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-19 17:27:05 +0530

    Create StringReplace.java

---
+++ StringReplace.java
@@ -0,0 +1,10 @@
+class StringReplace
+{
+public static void main(String args[])
+{
+String name= "Hello, Everyone";
+String replace = name.replace("e","a");
+System.out.println("original String " +name);
+System.out.println("After replace String is " +replace);
+}
+}
