794c8559bcba255473ca8b07cdd603ea1bc3edb0
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-19 17:26:04 +0530

    Create StringIndex.java
    Finding string index

---
+++ StringIndex.java
@@ -0,0 +1,8 @@
+class StringIndex
+{
+public static void main(String args[])
+{
+String name= "Banana";
+System.out.println("A index position in first occurence in BANANA is :- " +name.indexOf('a'));
+}
+}
