491cec7ca1c58683a652312bff42e2094de087e2
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-19 17:28:33 +0530

    Create StringSub.java
    Finding the sub string of the given string

---
+++ StringSub.java
@@ -0,0 +1,13 @@
+class StringSub
+{
+public static void main(String args[])
+{
+String name= "java programming";
+ String Sub = "programming";
+ boolean result=name.contains(Sub);
+if(result==true)
+{
+System.out.println("The String "+Sub+" is present in String");
+}
+}
+}
