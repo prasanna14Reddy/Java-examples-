f7c8b4ba9b2c6f426c2f3d61b6137c243f7c6581
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-05-24 20:27:59 +0530

    Create A.java
    Inheritance example

---
+++ A.java
@@ -0,0 +1,35 @@
+class A
+{
+	private void a()
+	{
+		System.out.println("A");
+	}
+	protected void a1() {
+		System.out.println("A1");
+	}
+	public void a2()
+	{
+		System.out.println("A2");
+	}
+}
+class B extends A
+{
+	void b()
+	{
+		System.out.println("B");
+	}
+}
+
+public class Inher_ex {
+
+	public static void main(String[] args) {
+
+     B ob = new B();
+     //ob.a();//private member
+     ob.a1();
+     ob.a2();
+     ob.b();
+
+	}
+
+}
