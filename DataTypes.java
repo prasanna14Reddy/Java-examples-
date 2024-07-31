1589a94f7cec32ba8ebbabdff0de3bd6b4e8047b
prasanna14Reddy <168544402+prasanna14Reddy@users.noreply.github.com>
2024-07-31 17:15:18 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes
+{
+public static void main(String args[])
+{
+char a='R';
+int b=77;
+byte c=4;
+short d=45;
+double e=4.3556;
+float f=4.733f;
+long g=12431;
+System.out.println("character: "+a);
+System.out.println("integer: "+b);
+System.out.println("byte: "+c);
+System.out.println("short: "+d);
+System.out.println("double: "+e);
+System.out.println("float: "+f);
+System.out.println("long: "+g);
+}
+}
